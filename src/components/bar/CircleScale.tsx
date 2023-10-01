d2d4501e9da9757156b1e5948c8a95a251b45e2c
GerasimGerasimov <dialix@yandex.ru>
2023-10-01 21:47:34 +0700

    1) Добавил индикатору свойство color 2) стиль CSS для SVG с анимацией изменений, интересный эффект "плавности хода"

--- src/components/bar/CircleScale.tsx
+++ src/components/bar/CircleScale.tsx
@@ -17,7 +17,8 @@ function getCircleLenght(R: number): number {
 
 interface ICircleScaleProps {
   ratio: number,
-  text: string
+  text: string,
+  color: string
 }
 
 export default class CircleScale extends Component <ICircleScaleProps, ICircleScaleState> {
@@ -46,8 +47,10 @@ export default class CircleScale extends Component <ICircleScaleProps, ICircleSc
             width="100%" height="100%"
             viewBox="0 0 100 100">
             <g transform='rotate(270 50 50)'>
-              <circle cx="50" cy="50" r={this.state.Radius}
-              stroke="#673E93" strokeWidth="12"
+              <circle 
+              className="transition-all-eo-1s"
+              cx="50" cy="50" r={this.state.Radius}
+              stroke={this.props.color} strokeWidth="12"
               strokeDasharray = {this.state.CircleLenghtStr}
               strokeDashoffset= {this.getArcLenghtByPrc(this.props.ratio)}
               fill="transparent"/>
@@ -67,4 +70,5 @@ export default class CircleScale extends Component <ICircleScaleProps, ICircleSc
 /*
              stroke-dasharray ="628"
              stroke-dashoffset="650"
+             stroke="#673E93"
 */
--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -4,7 +4,8 @@ import '../../css/styles.css'
 import CircleScale from '../../components/bar/CircleScale';
 
 interface IInfo {
-  ratio:number
+  ratio:number,
+  color:string
 }
 
 export default class InfoBlock extends Component <{}, IInfo> {
@@ -14,7 +15,8 @@ export default class InfoBlock extends Component <{}, IInfo> {
   constructor (props?:any) {
     super(props);
     this.state = {
-      ratio: 60
+      ratio: 60,
+      color:"RED"
     }
   }
 
@@ -33,11 +35,23 @@ export default class InfoBlock extends Component <{}, IInfo> {
     clearInterval(this.updateTimer);
   }
 
+  private handleRedBtn(e: any){
+    this.setState({color:"Red"});
+  }
+
+  private handleGreenBtn(e: any){
+    this.setState({color:"Green"});
+  }
+
   render() {
     return(
       <div className='box'>
         <CircleScale ratio={100 - this.state.ratio * (10/6)}
-                     text= {this.state.ratio.toString()}/>
+                     text= {this.state.ratio.toString()}
+                     color = {this.state.color}/>
+        <button onClick={(e)=>this.handleRedBtn(e)}>RED</button>
+        <button onClick={(e)=>this.handleGreenBtn(e)}>GREEN</button>
+        <p>{this.state.color}</p>
       </div>
     )
   }
